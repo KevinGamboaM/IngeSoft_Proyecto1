6dc472156d2db0802ad57c34d3e562e392e264c6
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-03-06 14:58:30 -0400

    Implementamos mostrar el precio en nuestra IU

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import totalizador from "./totalizador";
 
 const cantidad = document.querySelector("#cantidad");
+const precio = document.querySelector("#precio");
 const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
   
@@ -8,11 +9,11 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  //const cant = Number.parseInt(cantidad.value);
+  const prec = Number.parseInt(precio.value);
   
   const totalizar = new totalizador(cantidad.value);
-  
-  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + "</p>";
+
+  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + prec + "</p>";
   
   
 });
