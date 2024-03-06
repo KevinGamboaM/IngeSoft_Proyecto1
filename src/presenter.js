a9e1aa0cc15466f0c2a84172d6b404bc84654031
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-03-06 16:01:29 -0400

    Implementamos en nuestra IU mostrar el impuesto de UT usando el metodo get_impuesto de nuestra clase

--- src/presenter.js
+++ src/presenter.js
@@ -6,7 +6,7 @@ const estado = document.querySelector("#estado");
 const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
 const div2 = document.querySelector("#resultado-div2");  
-
+const div3 = document.querySelector("#resultado-div3");  
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -16,5 +16,5 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + "</p>";
   div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
-  
+  div3.innerHTML = "<p>Impuesto para " + totalizar.get_estado() + "(" + (totalizar.get_impuesto()*100) + "%)-->" + totalizar.get_precio_neto()*totalizar.get_impuesto() + "</p>";
 });
