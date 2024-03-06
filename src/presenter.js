a7f28916c5c50878f5f3053962314f386ac0b469
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-03-06 18:47:46 -0400

    Implementamos mostrar en pantalla el descuento que se tiene segun la cantidad que se comprar

--- src/presenter.js
+++ src/presenter.js
@@ -7,7 +7,8 @@ const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
 const div2 = document.querySelector("#resultado-div2");  
 const div3 = document.querySelector("#resultado-div3");
-const div4 = document.querySelector("#resultado-div4");    
+const div4 = document.querySelector("#resultado-div4");  
+const div5 = document.querySelector("#resultado-div5");  
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -18,5 +19,6 @@ form.addEventListener("submit", (event) => {
   div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + "</p>";
   div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
   div3.innerHTML = "<p>Impuesto para " + totalizar.get_estado() + "(" + (totalizar.get_impuesto()*100) + "%)-->" + totalizar.get_precio_neto()*totalizar.get_impuesto() + "</p>";
+  div5.innerHTML = "<p>Descuento(" + parseInt(totalizar.get_descuento()*100) + "%)" + totalizar.get_precio_neto() * totalizar.get_descuento() + "</p>";
   div4.innerHTML = "<p>PRECIO TOTAL: " + totalizar.get_total() + "</p>";
 });
