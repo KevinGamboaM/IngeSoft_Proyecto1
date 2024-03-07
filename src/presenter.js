73f513b84bb476db8d464222ac98d92a93f8c919
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-03-06 21:05:45 -0400

    Implementamos ingresar el peso y mostrarlo en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,7 @@ const cantidad = document.querySelector("#cantidad");
 const precio = document.querySelector("#precio");
 const estado = document.querySelector("#estado");
 const categoria = document.querySelector("#categoria");
+const peso = document.querySelector("#peso");
 const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
 const div2 = document.querySelector("#resultado-div2");  
@@ -15,11 +16,11 @@ const div6 = document.querySelector("#resultado-div6");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  //const cat = categoria.value;
+  const p = peso.value;
   
   const totalizar = new totalizador(cantidad.value, precio.value, estado.value,categoria.value);
 
-  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + " | Categoria: " + totalizar.get_categoria() + "</p>";
+  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + " | Categoria: " + totalizar.get_categoria() + " | Peso: " + p + "</p>";
   div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
   div3.innerHTML = "<p>Impuesto para " + totalizar.get_estado() + "(" + (totalizar.get_impuesto()*100) + "%)-->" + totalizar.get_precio_neto()*totalizar.get_impuesto() + "</p>";
   div5.innerHTML = "<p>Descuento(" + parseInt(totalizar.get_descuento()*100) + "%)" + totalizar.get_precio_neto() * totalizar.get_descuento() + "</p>";
