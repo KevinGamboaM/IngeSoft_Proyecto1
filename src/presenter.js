8e54b8353d5481735285903adec01c05be2cf5cb
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-03-06 20:05:49 -0400

    Implementamos a nuestra IU mostrar el descuento adicional segun la categoria de alimentos, varios y bebidas alcoholicas

--- src/presenter.js
+++ src/presenter.js
@@ -10,18 +10,20 @@ const div2 = document.querySelector("#resultado-div2");
 const div3 = document.querySelector("#resultado-div3");
 const div4 = document.querySelector("#resultado-div4");  
 const div5 = document.querySelector("#resultado-div5");  
+const div6 = document.querySelector("#resultado-div6");  
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const cat = categoria.value;
+  //const cat = categoria.value;
   
-  const totalizar = new totalizador(cantidad.value, precio.value, estado.value);
+  const totalizar = new totalizador(cantidad.value, precio.value, estado.value,categoria.value);
 
-  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + " | Categoria: " + cat + "</p>";
+  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + " | Categoria: " + totalizar.get_categoria() + "</p>";
   div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
   div3.innerHTML = "<p>Impuesto para " + totalizar.get_estado() + "(" + (totalizar.get_impuesto()*100) + "%)-->" + totalizar.get_precio_neto()*totalizar.get_impuesto() + "</p>";
   div5.innerHTML = "<p>Descuento(" + parseInt(totalizar.get_descuento()*100) + "%)" + totalizar.get_precio_neto() * totalizar.get_descuento() + "</p>";
+  div6.innerHTML = "<p>Impuesto adicional para " + totalizar.get_categoria() + "(" + parseInt(totalizar.get_impuesto_adicional()*100) + "%)-->" + totalizar.get_precio_neto()*totalizar.get_impuesto_adicional() + "</p>";
   div4.innerHTML = "<p>PRECIO TOTAL: " + totalizar.get_total() + "</p>";
   
 });
