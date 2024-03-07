0decbfed4927f92032982fb456ee74f15da71bef
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-03-06 22:32:34 -0400

    Implementamos mostrar el tipo de cliente en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,7 @@ const cantidad = document.querySelector("#cantidad");
 const precio = document.querySelector("#precio");
 const estado = document.querySelector("#estado");
 const categoria = document.querySelector("#categoria");
+const cliente = document.querySelector("#cliente");
 const peso = document.querySelector("#peso");
 const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
@@ -16,11 +17,11 @@ const div6 = document.querySelector("#resultado-div6");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
- // const p = peso.value;
+ const c = cliente.value;
   
   const totalizar = new totalizador(cantidad.value, precio.value, estado.value, categoria.value, peso.value);
 
-  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + " | Categoria: " + totalizar.get_categoria() + " | Peso: " + totalizar.get_peso() + "</p>";
+  div.innerHTML = "<p>Cantidad: " + totalizar.get_cantidad() + " | Precio: " + totalizar.get_precio() + " | Estado: " + totalizar.get_estado() + " | Categoria: " + totalizar.get_categoria() + " | Peso: " + totalizar.get_peso() + " | Tipo de cliente: " + c + "</p>";
   div2.innerHTML = "<p>Precio neto: " + totalizar.get_precio_neto() + "</p>";
   div3.innerHTML = "<p>Impuesto para " + totalizar.get_estado() + "(" + (totalizar.get_impuesto()*100) + "%)-->" + totalizar.get_precio_neto()*totalizar.get_impuesto() + "</p>";
   div5.innerHTML = "<p>Descuento(" + parseInt(totalizar.get_descuento()*100) + "%)" + totalizar.get_precio_neto() * totalizar.get_descuento() + "</p>";
